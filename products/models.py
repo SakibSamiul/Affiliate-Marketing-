ceb5fa115dad9ec18147d205d62e2cbaab11a393
SakibSamiul <sb.sakib77@gmail.com>
2025-01-06 23:06:59 +0600

    created a class name product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField()
+    image_url = models.URLField()
+    affiliate_link = models.URLField()
+
+    def __str__(self):
+        return self.name
